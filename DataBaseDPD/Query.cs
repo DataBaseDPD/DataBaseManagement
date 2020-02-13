7e68fd7e963f3b323bbde1894c199410128ef668
David Negrete Suarez <dnegrete001@ikasle.ehu.eus>
2020-02-13 13:25:57 +0100

    Add Error class and some methods

--- DataBaseDPD/Query.cs
+++ DataBaseDPD/Query.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace DataBaseDPD
 {
-    class Query
+     class Query 
     {
+        // Constructor that takes no arguments:
+        private Query()
+        {
+            string statement = "unknown";
+        }
+        // Constructor that takes one argument:
+        private Query (string statement)
+        {
+            checkValidity(statement);
+            
+        }
+
+        private void checkValidity(string statement)
+        {
+            throw new NotImplementedException();
+        }
+        Table executeQuery()
+        {
+            return new Table();
+        }
+        int executeUpDate()
+        {
+            return -1;
+        }
     }
+   
+    
+    
 }
