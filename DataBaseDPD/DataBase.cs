26a15e69f5f4144db4c32b02e96cfe13c549b48b
David Negrete <dnegrete001@ikasle.ehu.eus>
2020-03-31 22:04:56 +0200

    Merge

--- DataBaseDPD/DataBase.cs
+++ DataBaseDPD/DataBase.cs
@@ -94,11 +94,7 @@ namespace DataBaseDPD
             }
 
         }
-        private Boolean close()
-        {
-            //Before close we have to save the changes
-            return false;
-        }
+       
 
 
         public Table load(string fileName)
