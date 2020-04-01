4931406475b52a115b16f2ca925d9b1a15bdfe24
Domenico Michele Romero <domenix@outlook.com>
2020-04-01 19:47:10 +0200

    InsertClass

--- DataBaseDPD/Insert.cs
+++ DataBaseDPD/Insert.cs
@@ -1,8 +1,78 @@
 ﻿using System;
+using DataBaseDPD;
 
-public class Class1
+namespace DataBaseDPD
 {
-	public Class1()
-	{
-	}
+
+    public class Insert : Query
+    {
+
+        List<string> columNames = new List<string>();
+        string[] valuesSeparated;
+        string tabName { get; }
+
+
+        //Sin columna
+        public Insert(string table, string values)
+        {
+
+            tabName = table;
+            valuesSeparated = values.Split(',');
+            for (int a=0;a<valuesSeparated.Length;a++)
+            {
+                valuesSeparated[a] = valuesSeparated[i].Trim(' ');    
+            }
+        }
+
+        //Con columna
+        public Insert (string table, string columns , string values)
+        {
+
+            string[] listColumns = columns.Split(',');
+            foreach (string col in listColumns)
+            {
+
+                columNames.Add(col);
+
+            }
+
+            tabName = table;
+            valuesSeparated = values.Split(',');
+            for (int a = 0; a < valuesSeparated.Length; a++)
+            {
+                valuesSeparated[a] = valuesSeparated[i].Trim(' ');
+            }
+
+        }
+
+        public override string Run(DataBase db)
+        {
+
+            if (columNames.Count ==0)
+            {
+
+                db.Insert(tabName,valuesSeparated);
+
+
+            }
+            else
+            {
+
+
+                db.Insert(tabName,columNames,valuesSeparated);
+
+
+            }
+
+            return null;
+
+        }
+
+
+    }
+
+
+
+
+
 }
