feb44ba78aec2ad854abeea7206fe0d5ea8c4419
David Negrete <dnegrete001@ikasle.ehu.eus>
2020-04-01 20:26:53 +0200

    #4 Parer change

--- DataBaseDPD/Parser.cs
+++ DataBaseDPD/Parser.cs
@@ -5,117 +5,135 @@ using System.Collections.Generic;
 namespace DataBaseDPD
 {
 	public class Parser
+
 	{
-		public static Query Parse(string query)
+
+        public static Query Parse(string query)
 		{
 
-			const string select = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|\<|\>)\s+(\w+))?(\;)";
-			const string update = @"UPDATE\s+(\w+)\s+SET\s+([\w\,\=\@\.]+)\s+WHERE\s+(\w+)\s*(=|<|>)\s*(\w+);";
-			const string delete = @"DELETE\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|<|>)\s+(\w+))?(\;)";
-			const string insert = @"INSERT\s+INTO\s+(\w+)\s+\(([^\)]+)\)\s+VALUES\s+\(([^\)]+)\);";
-			 
-
-
-			const string createDataBase = @"CREATE DATABASE\s+(|\w+)(\;)";
-			const string dropDataBase = @"DROP DATABASE\s+(\w+)(\;)";
-			const string dropTable = @"DROP TABLE\s+(\*|\w+)(\;)";
-			const string createTable = @"CREATE TABLE\s+(\w+)\s+\(([^\)]+)\)\s*\;";
-
-
-
-
-			//Select
-			Match match = Regex.Match(query, select);
-			if (match.Success)
-			{
-				//mirar la lista
-				string columns = match.Groups[1].Value;
-				string table = match.Groups[2].Value;
-				string left = match.Groups[3].Value;
-				string op = match.Groups[4].Value;
-				string right = match.Groups[5].Value;
-				return new Select();
-			}
-
-			//Insert con columnas
-			match = Regex.Match(query, insert);
-			if (match.Success)
-			{
-				string table = match.Groups[1].Value;
-				string columns = match.Groups[2].Value;
-				string values = match.Groups[3].Value;
-
-				return new Insert(table, columns, values);
-			}
-
-			//Insert sin columnas
-
-
-
+			// SELECT
+			//Con *:
+			const string select1 = @"SELECT\s+(\*)\s+FROM\s+(\w+);";
+			const string select2 = @"SELECT\s+(\w+)\s+FROM\s+(\w+);";
+            
+			
+			//Insert
+			const string insert1 = @"INSERT\s+INTO\s+(\w+)\s+VALUES\s+\(([\w\'\s+\.]+)\);"; //CON TODOS SUS VALUES(1)
 
 			//Delete
-			match = Regex.Match(query, delete);
-            if (match.Success)
-            {
-				string table = match.Groups[1].Value;
-				string left = match.Groups[2].Value;
-				string op = match.Groups[3].Value;
-				string right = match.Groups[4].Value;
-				return new Delete();
-            }
+			const string delete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+)\s*(=|<|>)\s*(\w+);";
 
-			//CreateTable
+			//Update
+			const string update1 = @"UPDATE\s+(\w+)\s+SET\s+(\w+)\s*(\=)\s*(\w+)(\,\s+(\w+)\s*(\=)\s*(\w+)\s+)WHERE\s+(\w+)\s*(=|<|>)\s*(\w+);"; //no traga comillas en los attbs ni espacios
 
-			match = Regex.Match(query, createTable);
-            if(match.Success)
-				{
+			
 
-				string nombreTabla = match.Groups[1].Value;
-				string tipoDato = match.Groups[2].Value;
-				return new CreateTable(nombreTabla, tipoDato);
+			//Drop DB 
+			const string dropDB = @"DROP\s+DATABASE\s+(\w+);";
 
-            }
+			//Create DB
+			const string createDB = @"CREATE\s+DATABASE\s+(\w+);";
 
-			//DropTable
+			//Create Table
+			//string createTable = @"(CREATE\s+TABLE)\s+(\w+)\s+\((\w+)\s+(INT|DOUBLE|TEXT)(\s+|\,\s+\w+\s+(INT|DOUBLE|TEXT))+)\,\s+(PRIMARY\s+KEY)\s+\((\w+)\)\,\s+(FOREIGN\s+KEY)\s+\((\w+)\)\s+REFERENCES\s+(\w+)\s+\((\w+)\);";
+			const string createTable1 = @"CREATE\s+TABLE\s+(\w+)\s+\((\w+)\s+(INT|DOUBLE|TEXT)\);";
+			const string createTable2 = @"CREATE\s+TABLE\s+(\w+)\s+\((\w+)\s+(INT|DOUBLE|TEXT)(\,\s+(\w+)\s+(INT|DOUBLE|TEXT))+\);";
+			//Drop Table
+			const string dropTable = @"DROP\s+TABLE\s+(\w+);";
 
+           ////__-------------------------------------------------------------------------------------------
 
-
-
-
+			//Select With where
+			const string select3 = @"SELECT\s+(\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+)\s*(=|<|>)\s*([\w\']+);";
+			//Update with Where
+			const string update2 = @"UPDATE\s+(\w+)\s+SET\s+(\w+)\s*(\=)\s*(\w+)\s+WHERE\s+(\w+)\s*(=|<|>)\s*(\w+);";
 
 
-			//CreateDatabase
 
-			match = Regex.Match(query,createDataBase);
-            {
-
-                if (match.Success)
-                {
-
-					string nombreDB = match.Groups[0].Value;
-					return new CreateDataBase(nombreDB);
-
-                }
-
-            }
-
-
-			//DropDatabase
-
-			match = Regex.Match(query, dropDataBase);
-
+			//Select all from table
+			Match match = Regex.Match(query, select1);
+			if (match.Success)
+			{
+				string table = match.Groups[2].Value;
+				return new SelectAll(table);
+			}
+            //Select columns
+			match = Regex.Match(query, select2);
 			if(match.Success)
             {
+				List<string> columnNames = CommaSeparatedNames(match.Groups[1].Value);
+				string table = match.Groups[2].Value;
+				return new Select(table, columnNames);
 
-				string nombreDB = match.Groups[0].Value;
-				return new DropDataBase(nombreDB);
+			}
 
-            }
+			// Insert todos valores
+			 match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+                //TODO
+				return null;
+
+			}
+
+			// Delete
+			match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+				//TODO
+				return null;
+
+			}
+			match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+				//TODO
+				return null;
+
+			}
+			match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+				//TODO
+				return null;
+
+			}
+			match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+				//TODO
+				return null;
+
+			}
+			match = Regex.Match(query, insert1);
+			if (match.Success)
+			{
+				//TODO
+				return null;
+
+			}
 
 
 
 
 
+
+
+
+
+
+			return null;
+
+		}
+		static List<string> CommaSeparatedNames(string text)
+		{
+			List<string> result = new List<string>();
+			string[] separate = text.Split(',');
+			for (int i = 0; i < separate.Length; i++)
+			{
+				result.Add(separate[i].Trim(' '));
+			}
+			return result;
 		}
 	}
 }
