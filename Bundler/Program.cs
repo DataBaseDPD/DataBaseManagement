7616b9c60a207e9e7f00c0718dba5f8e6d2251bf
Domenix90 <domenix@outlook.com>
2020-05-01 17:49:07 +0200

    Bundler

---
+++ Bundler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bundler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
