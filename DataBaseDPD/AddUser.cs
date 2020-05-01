51b9f0efc53d4195fb3ce3e708b298e573c96dcd
David Negrete <dnegrete001@ikasle.ehu.eus>
2020-05-01 11:23:04 +0200

    To pass test

---
+++ DataBaseDPD/AddUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBaseDPD
+{
+    public class AddUser : Query
+    {
+        string User;
+        string Passwrd;
+        string Profile;
+
+        public AddUser(List<string> data)
+        {
+            try
+            {
+                User = data[0];
+                Passwrd = data[1];
+                Profile = data[2];
+            }
+            catch (Exception  e)
+            {
+                Console.WriteLine("Wrong data to add user.");
+                Console.WriteLine(e.StackTrace);
+            }
+            
+        }
+
+        public override string getTableName()
+        {
+            return null;
+        }
+
+        public override PrivilegeType getType()
+        {
+            return PrivilegeType.ADMIN;
+        }
+
+        public override string Run(Database database)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
