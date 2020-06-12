a81e05e41f3f238381619c59d2e313071356a69f
Domenico Michele Romero <domenix@outlook.com>
2020-06-12 19:51:48 +0200

    TCPListener, version 1 ,need fix some bugs

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,28 +11,34 @@ namespace Client
         {
             try
             {
-
+                //Create a TCP Client
+                //Note , for this client to work you need to haave a TcpServer
+                //connected to same address as specified by the server, port
+                //combination
                 Int32 port = 13000;
                 String server = "127.0.0.1";
                 TcpClient client = new TcpClient(server, port);
 
-
+                //Translate the passed message into ASCII and store it as a Byte arry
                 Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
+                //Get a client stream for reading and writing 
                 NetworkStream stream = client.GetStream();
 
-
+                //send the message to the conected Tcp server
                 stream.Write(data, 0, data.Length);
 
                 Console.WriteLine("Sent: {0}", message);
 
+                //Recive the TcpServer.response.
 
+                //Buffer to store the response bytes
                 data = new Byte[256];
 
-
+                //String to store the response ASCII representation
                 String responseData = String.Empty;
 
-
+                //Read the first Batch of the TcpServer response bytes
                 Int32 bytes = stream.Read(data, 0, data.Length);
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
                 Console.WriteLine("Received: {0}", responseData);
@@ -63,10 +69,10 @@ namespace Client
         public static void Main(string[] args)
         {
 
-
+            
             Console.Write("Connect: ");
             string connection = Console.ReadLine();
-
+            //We need put data information whit comas
             List<string> result = new List<string>();
             string[] separate = connection.Split(',');
             for (int i = 0; i < separate.Length; i++)
@@ -74,7 +80,7 @@ namespace Client
                 result.Add(separate[i].Trim(' '));
             }
             string message = string.Format("<Open Database={0} User={1} Password={2}/>", result[0], result[1], result[2]);
-
+            //start connection TcpClient
             String res = Connect(message);
 
             if (res.Equals("<Success/>"))
--- DBConsole/Program.cs
+++ DBConsole/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
 using DataBaseDPD;
 
 namespace DBConsole
@@ -10,128 +10,138 @@ namespace DBConsole
     class Program
     {
 
-        static void Main()
+        class Server
         {
-            TcpListener server = null;
+            public static Connection con = new Connection();
 
-
-            try
+            //ParserQuery is a function that treat the answer and query
+           
+            public static String ParseQuery(String message)
             {
-               
-                Int32 port = 1200;
-                string ip = "127.0.0.1";
-                IPAddress localAddr = IPAddress.Parse(ip);
 
-                Byte[] bytes = new Byte[256];
-                String data = null;
-                String input = null;
-                bool isCorrectIpAndPort = false;
+                Match match;
+                String response = null;
+                //regular expressions
+                const string connection = @"<Open\sDatabase(=)([^\)]+)\sUser(=)([^\)]+)\sPassword(=)\s*([^\)]+)/>";
+                const string query = @"<Query>(\w+)</Query>";
 
-
-                server = new TcpListener(localAddr, port);
-
-                //connect to bd storage
-                Connection con = new Connection();
-
-
-                // Start listening for client requests.
-                server.Start();
-
-
-                //TcpClient client = server.AcceptTcpClient();
-
-                TcpClient client = new TcpClient();
-
-
-                NetworkStream stream = client.GetStream();
-
-
-                input = null;
-                int j;
-
-                // Process the first input of the client to check if all is correct
-                while ((j = stream.Read(bytes, 0, bytes.Length)) != 0)
+                match = Regex.Match(message, query);
+                if (match.Success)
                 {
-                    // Translate data bytes to a ASCII string.
-                    input = Encoding.ASCII.GetString(bytes, 0, j);
+                    string res = con.RunQuery(match.Groups[1].Value);
+
+                    if (res.StartsWith("ERROR"))
+                    {
+                        response = string.Format("<Answer><Error>{0}</Error></Answer>", res);
+                    }
+                    else
+                    {
+                        response = string.Format("<Answer>{0}</Answer>", res);
+                    }
+                    return response;
+                }
+                match = Regex.Match(message, connection);
+                if (match.Success)
+                {
+                    string database = (string)match.Groups[1].Value;
+                    string user = (string)match.Groups[2].Value;
+                    string password = (string)match.Groups[3].Value;
+
+                    string res = con.Connect(database, user, password);
 
 
-                    byte[] msg = Encoding.ASCII.GetBytes(input);
-
-
-
-                    if (port == 0 && ip == "") isCorrectIpAndPort = true;
-
-                    //Try to connect to DataBase
-                    //Parameter: Database, user, password
-                    con.Connect("","","");
-
-                    // Send back a response.
-                    stream.Write(msg, 0, msg.Length);
-                    
+                    if (con.isConnected())
+                    {
+                        response = "<Success/>";
+                    }
+                    else
+                    {
+                        response = string.Format("<Error>{0}</Error>", res);
+                    }
+                    return response;
                 }
 
+                return response;
+
+            }
 
 
-                if (con.isConnected()&& isCorrectIpAndPort)
+
+            public static void Main(string[] args)
+            {
+                //Listens for TCP network client connections
+                TcpListener server = null;
+                try
                 {
-                    Console.WriteLine("Connected!");
-                    // Enter the listening loop.
+                   //Set the Tcp Listener on port 13000
+                    Int32 port = 13000;
+                    string ip = "127.0.0.1";
+                    IPAddress localAddr = IPAddress.Parse(ip);
+
+                    //Create a new server and put it on server variable
+                    server = new TcpListener(localAddr, port);
+
+                    //star server
+                    server.Start();
+
+                    //buffer for reading data, and define response 
+                    Byte[] bytes = new Byte[256];
+                    String data = null;
+                    String response = null;
+
+                    //enter the listening loop
                     while (true)
                     {
-                        Console.Write("Waiting for a response... ");
+                        Console.Write("Waiting for a connection... ");
+
+                        //Perform a blocking call to acept request
+                        //You could also use server.AcceptSocket() here.
+                        TcpClient client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
 
                         data = null;
+
+                        //Get a stream object for reading and writing
+                        NetworkStream stream = client.GetStream();
+
                         int i;
 
-                        // Loop to receive all the data sent by the client.
+                        //Loop to recive all the data sent by client
                         while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                         {
-                            // Translate data bytes to a ASCII string.
+                            //Translate data bytes to a ASCII string
                             data = Encoding.ASCII.GetString(bytes, 0, i);
+                            Console.WriteLine("Received: {0}", data);
+
+                            //We call ParserQuery , proccess the data sent by the client
+                            response = ParseQuery(data);
 
 
-                            byte[] msg = Encoding.ASCII.GetBytes(data);
+                            byte[] msg = Encoding.ASCII.GetBytes(response);
 
-                            //process the queries over database
-                            string response = con.RunQuery("");
-
-
-                            // Send back a response.
+                            //send back a response
                             stream.Write(msg, 0, msg.Length);
-                            
+                            Console.WriteLine("Sent: {0}", response);
                         }
 
+                        //shutdown and end connections
+                        client.Close();
                     }
-
-
                 }
-                else
+                catch (SocketException e)
                 {
-                    Console.WriteLine("Not connected!");
-                    con.Close();
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+                finally
+                {
+
+                    server.Stop();
                 }
 
+                Console.WriteLine("\nHit enter to continue...");
+                Console.Read();
             }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-                
-            }
-
-
-
 
         }
-
-
-
-
-
     }
 }
